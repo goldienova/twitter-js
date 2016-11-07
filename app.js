adf4ffcfd2f85637d7802ff15ca7e2c69c2a2634
Kathryn Middleton <kmiddleton14@gmail.com>
2016-11-07 17:59:24 -0500

    added middleware

--- app.js
+++ app.js
@@ -10,3 +10,28 @@ app.get('/', function(request, response){
 	response.send("this is a response to get");
 
 })
+
+app.get('/news', function(request, response){
+	response.send("this is a NEWWWW response to get");
+
+})
+
+app.use('/test', function(req,res, next){
+	console.log(req.method + " " + req.originalUrl);
+	next();
+})
+
+app.get('/test', function(request, response){
+	response.send("this is another response to get");
+
+})
+
+app.post('/test', function(request, response){
+	response.send("this is a POST response to get");
+
+})
+
+app.all('/special', function(req,res){
+	response.send("you've reached the special area");
+	
+})
