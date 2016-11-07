547edfc6fcd05364ee9c79557a2f528b59dcce87
Kathryn Middleton <kmiddleton14@gmail.com>
2016-11-07 17:19:26 -0500

    added a get handler

--- app.js
+++ app.js
@@ -5,3 +5,8 @@ var app = express();
 app.listen(3000, function () {
 	console.log("server listening");
 });
+
+app.get('/', function(request, response){
+	response.send("this is a response to get");
+
+})
