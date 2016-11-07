ca083c6626e6b6ea8c8811862c72d341ec0c66b5
Mariana Templin <mariana.acuna.templin@gmail.com>
2016-11-07 18:22:12 -0500

    added nunjucks

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 var express = require('express');
+var nunjucks = require('nunjucks');
+
+var locals = {
+	title: "An Example",
+	people: [
+		{name: 'Gandalf'},
+		{name: 'Frodo'},
+		{name: 'Hermione'}
+	]
+};
+
+nunjucks.configure('views', {noCache: true});
+nunjucks.render('index.html', locals, function(err, output) {
+	console.log(output);
+});
 
 var app = express();
 
