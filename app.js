d86ca632faa13304bc9f4bc67d65463b05f5ad9a
Kathryn Middleton <kmiddleton14@gmail.com>
2016-11-08 11:51:32 -0500

    added router index and css file templates

--- app.js
+++ app.js
@@ -2,59 +2,51 @@ var express = require('express');
 var nunjucks = require('nunjucks');
 var app = express();
 
+
+const routes = require('./routes/');
+app.use('/', routes);
+
 app.set('view engine', 'html');
 app.engine('html', nunjucks.render);
 nunjucks.configure('views', {noCache: true});//caching is valuable 
 //but can slow us down during development so it is turned off for now
 
 
-// var locals = {
-// 	title: "An Example",
-// 	people: [
-// 		{name: 'Gandalf'},
-// 		{name: 'Frodo'},
-// 		{name: 'Hermione'}
-// 	]
-// };
 
-
-// nunjucks.render('index.html', locals, function(err, output) {
-// 	console.log(output);
-// });
-
-const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+//const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
 app.listen(3000, function () {
 	console.log("server listening");
 });
-
-app.get('/', function(request, response){
-	response.render('index', {title: 'Hall of Fame', people: people})
-	//response.send("this is a response to get");
-
-})
-
-app.get('/news', function(request, response){
-	response.send("this is a NEWWWW response to get");
-
-})
 
 app.use('/test', function(req,res, next){
 	console.log(req.method + " " + req.originalUrl);
 	next();
 })
 
-app.get('/test', function(request, response){
-	response.send("this is another response to get");
 
-})
 
-app.post('/test', function(request, response){
-	response.send("this is a POST response to get");
 
-})
+// app.get('/', function(request, response){
+// 	response.render('index', {title: 'Hall of Fame', people: people})
+// 	//response.send("this is a response to get");
 
-app.all('/special', function(req,res){
-	response.send("you've reached the special area");
+// })
+
+// app.get('/news', function(request, response){
+// 	response.send("this is a NEWWWW response to get");
+
+// })
+
+
+
+
+// app.post('/test', function(request, response){
+// 	response.send("this is a POST response to get");
+
+// })
+
+// app.all('/special', function(req,res){
+// 	response.send("you've reached the special area");
 	
-})
+// })
--- tweetBank.js
+++ tweetBank.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const _ = require('lodash');
 
 var data = [];
@@ -15,3 +17,28 @@ function find(properties){
 }
 
 module.exports = {add: add, list: list, find: find};
+
+
+
+
+const randArrayEl = function(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const getFakeName = function() {
+  let fakeFirsts = ['Nimit', 'David', 'Shanna', 'Emily', 'Scott', 'Karen', 'Ben', 'Dan', 'Ashi', 'Kate', 'Omri', 'Gabriel', 'Joe', 'Geoff'];
+  let fakeLasts = ['Hashington', 'Stackson', 'McQueue', 'OLogn', 'Ternary', 'Claujure', 'Dunderproto', 'Binder', 'Docsreader', 'Ecma'];
+  return randArrayEl(fakeFirsts) + " " + randArrayEl(fakeLasts);
+};
+
+const getFakeTweet = function() {
+  let awesome_adj = ['awesome', 'breathtaking', 'amazing', 'funny', 'sweet', 'cool', 'wonderful', 'mindblowing', 'impressive'];
+  return "Fullstack Academy is " + randArrayEl(awesome_adj) + "! The instructors are just so " + randArrayEl(awesome_adj) + ". #fullstacklove #codedreams";
+};
+
+for (let i = 0; i < 10; i++) {
+  module.exports.add( getFakeName(), getFakeTweet() );
+}
+
+
+
