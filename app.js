4eec2513ab827311baf3029991155f918fb03d69
Mariana Templin <mariana.acuna.templin@gmail.com>
2016-11-08 10:32:30 -0500

    added html render

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 var express = require('express');
 var nunjucks = require('nunjucks');
-
-var locals = {
-	title: "An Example",
-	people: [
-		{name: 'Gandalf'},
-		{name: 'Frodo'},
-		{name: 'Hermione'}
-	]
-};
-
-nunjucks.configure('views', {noCache: true});
-nunjucks.render('index.html', locals, function(err, output) {
-	console.log(output);
-});
-
 var app = express();
+
+app.set('view engine', 'html');
+app.engine('html', nunjucks.render);
+nunjucks.configure('views', {noCache: true});//caching is valuable 
+//but can slow us down during development so it is turned off for now
+
+
+// var locals = {
+// 	title: "An Example",
+// 	people: [
+// 		{name: 'Gandalf'},
+// 		{name: 'Frodo'},
+// 		{name: 'Hermione'}
+// 	]
+// };
+
+
+// nunjucks.render('index.html', locals, function(err, output) {
+// 	console.log(output);
+// });
+
+const people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
 
 app.listen(3000, function () {
 	console.log("server listening");
 });
 
 app.get('/', function(request, response){
-	response.send("this is a response to get");
+	response.render('index', {title: 'Hall of Fame', people: people})
+	//response.send("this is a response to get");
 
 })
 
